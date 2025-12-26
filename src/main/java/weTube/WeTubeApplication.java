06e00dbe79eef7b2707632133d902c60f2cfb491
a-helena19 <helenaaldlol@gmail.com>
2025-12-26 14:28:50 +0100

    Initial commit

--- src/main/java/weTube/WeTubeApplication.java
+++ src/main/java/weTube/WeTubeApplication.java
@@ -1,4 +1,4 @@
-package com.example.wetube;
+package weTube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/weTube/WeTubeApplicationTests.java
+++ src/test/java/weTube/WeTubeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.wetube;
+package weTube;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
